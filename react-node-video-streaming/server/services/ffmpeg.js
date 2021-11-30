cc64a49ebc55d05a387852069f18c177a6c06029
quanhoang288 <quanpro288@gmail.com>
2021-12-01 02:19:05 +0700

    refactor services

--- react-node-video-streaming/server/services/ffmpeg.js
+++ react-node-video-streaming/server/services/ffmpeg.js
@@ -1,113 +1,108 @@
-const fs = require('fs');
 const path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
-const { UPLOAD_DIR } = require('../configs');
+const { ASSET_DIR } = require('../configs');
 
 const ffmpegServices = {};
 
-ffmpegServices.encode = file => {
-    return new Promise((resolve, reject) => {
-        const sizes = [
-            [240, 350],
-            [480, 700],
-            [720, 2500]
-        ];
-        const basename = path.basename(file.filename, path.extname(file.filename));
-        const sourceFn = file.path;
-        const targetDir = file.destination;
-        const targetFn = path.join(targetDir, `${basename}.mpd`);
-    
-        const startTime = Date.now();
-    
-        const ffmpegCmd = ffmpeg({
-            source: path.resolve(sourceFn),
-            cwd: path.resolve(targetDir)
-        });
-        ffmpegCmd
-            .format('dash')
-            .videoCodec('libx264')
-            .audioCodec('aac')
-            .audioChannels(2)
-            .audioFrequency(44100)
-            .outputOptions([
-                '-preset veryfast',
-                '-keyint_min 60',
-                '-g 60',
-                '-sc_threshold 0',
-                '-profile:v main',
-                '-use_template 1',
-                '-use_timeline 1',
-                '-b_strategy 0',
-                '-bf 1',
-                '-map 0:a?',
-                '-b:a 96k'
-            ]);
-    
-        for (let i = 0; i < sizes.length; i++) {
-            let size = sizes[i];
-            ffmpegCmd.outputOptions([
-                `-filter_complex [0]format=pix_fmts=yuv420p[temp${i}];[temp${i}]scale=-2:${size[0]}[A${i}]`,
-                `-map [A${i}]:v`,
-                `-b:v:${i} ${size[1]}k`,
-            ]);
-        }
-    
-        ffmpegCmd.on('start', commandLine => {
-            console.log('Spawned FFmpeg with command: ' + commandLine);
-        });
-    
-        ffmpegCmd.on('progress', info => {
-            console.log(`Encoding: ${info.percent}`);
-        })
-            .on('end', () => {
-                const endTime = Date.now();
-                console.log(`Encoding finished after ${(endTime - startTime) / 1000}s`);
-                return resolve(targetDir);
-            })
-            .on('error', err => {
-                console.log('error', err.message);
-                return reject(err);
-            })
-            .save(path.resolve(targetFn)); 
-    });
-}
+ffmpegServices.encode = (file) =>
+  new Promise((resolve, reject) => {
+    const sizes = [
+      [240, 350],
+      [480, 700],
+      [720, 2500],
+    ];
+    const basename = path.basename(file.filename, path.extname(file.filename));
+    const sourceFn = file.path;
+    const targetDir = file.destination;
+    const targetFn = path.join(targetDir, `${basename}.mpd`);
 
-ffmpegServices.generateThumbnail = file => {
-    return new Promise((resolve, reject) => {
-        const basename = path.basename(file.filename, path.extname(file.filename));
-        const sourceFn = file.path;
-        const targetDir = file.destination;
-        const thumbnailFileName = `${basename}-thumbnail.png`;
-        const targetFn = path.join(targetDir, thumbnailFileName);
-    
-        const ffmpegCmd = ffmpeg({
-            source: path.resolve(sourceFn),
-            cwd: path.resolve(targetDir)
-        });
-    
-        ffmpegCmd.on('start', commandLine => {
-            console.log('Generating thumbnail');
-            console.log('Spawned FFmpeg with command: ' + commandLine);
-        });
-    
-        ffmpegCmd
-            .seekInput('00:00:01')
-            .outputOptions([
-                '-frames:v 1'
-            ])
-            .on('end', () => {
-                return resolve({
-                    filename: thumbnailFileName,
-                    path: targetFn
-                }); 
-            })
-            .on('error', err => {
-                console.log('error', err.message);
-                return reject(err);
-            })
-            .save(path.resolve(targetFn));
+    const startTime = Date.now();
+
+    const ffmpegCmd = ffmpeg({
+      source: path.resolve(sourceFn),
+      cwd: path.resolve(targetDir),
     });
-    
-}
+    ffmpegCmd
+      .format('dash')
+      .videoCodec('libx264')
+      .audioCodec('aac')
+      .audioChannels(2)
+      .audioFrequency(44100)
+      .outputOptions([
+        '-preset veryfast',
+        '-keyint_min 60',
+        '-g 60',
+        '-sc_threshold 0',
+        '-profile:v main',
+        '-use_template 1',
+        '-use_timeline 1',
+        '-b_strategy 0',
+        '-bf 1',
+        '-map 0:a?',
+        '-b:a 96k',
+      ]);
+
+    for (let i = 0; i < sizes.length; i += 1) {
+      const size = sizes[i];
+      ffmpegCmd.outputOptions([
+        `-filter_complex [0]format=pix_fmts=yuv420p[temp${i}];[temp${i}]scale=-2:${size[0]}[A${i}]`,
+        `-map [A${i}]:v`,
+        `-b:v:${i} ${size[1]}k`,
+      ]);
+    }
+
+    ffmpegCmd.on('start', (commandLine) => {
+      console.log(`Spawned FFmpeg with command: ${commandLine}`);
+    });
+
+    ffmpegCmd
+      .on('progress', (info) => {
+        console.log(`Encoding: ${info.percent}`);
+      })
+      .on('end', () => {
+        const endTime = Date.now();
+        console.log(`Encoding finished after ${(endTime - startTime) / 1000}s`);
+        resolve({ targetDir, targetFn });
+      })
+      .on('error', (err) => {
+        console.log('error', err.message);
+        reject(err);
+      })
+      .save(path.resolve(targetFn));
+  });
+
+ffmpegServices.generateThumbnail = (file) =>
+  new Promise((resolve, reject) => {
+    const basename = path.basename(file.filename, path.extname(file.filename));
+    const sourceFn = file.path;
+    const targetDir = ASSET_DIR;
+    const thumbnailFileName = `${basename}-thumbnail.png`;
+    const targetFn = path.join(targetDir, thumbnailFileName);
+
+    const ffmpegCmd = ffmpeg({
+      source: path.resolve(sourceFn),
+      cwd: path.resolve(targetDir),
+    });
+
+    ffmpegCmd.on('start', (commandLine) => {
+      console.log('Generating thumbnail');
+      console.log(`Spawned FFmpeg with command: ${commandLine}`);
+    });
+
+    ffmpegCmd
+      .seekInput('00:00:01')
+      .outputOptions(['-frames:v 1'])
+      .on('end', () =>
+        resolve({
+          filename: thumbnailFileName,
+          path: targetFn,
+        }),
+      )
+      .on('error', (err) => {
+        console.log('error', err.message);
+        reject(err);
+      })
+      .save(path.resolve(targetFn));
+  });
 
 module.exports = ffmpegServices;
--- react-node-video-streaming/server/services/googleDrive.js
+++ react-node-video-streaming/server/services/googleDrive.js
@@ -3,63 +3,63 @@ const { google } = require('googleapis');
 const { GOOGLE_DRIVE_API_VERSION } = require('../configs');
 
 module.exports = class GoogleDriveService {
-	constructor(clientId, clientSecret, redirectUri, refreshToken) {
-		this.driveClient = this.createDriveClient(clientId, clientSecret, redirectUri, refreshToken); 
-	}
+  constructor(clientId, clientSecret, redirectUri, refreshToken) {
+    this.driveClient = this.createDriveClient(
+      clientId,
+      clientSecret,
+      redirectUri,
+      refreshToken,
+    );
+  }
 
-	createDriveClient = (clientId, clientSecret, redirectUri, refreshToken) => {
-		const client = new google.auth.OAuth2(
-			clientId,
-			clientSecret,
-			redirectUri,
-		);
-		client.setCredentials({ refresh_token: refreshToken });
-		return google.drive({
-			version: GOOGLE_DRIVE_API_VERSION,
-			auth: client
-		});
-	}
+  createDriveClient(clientId, clientSecret, redirectUri, refreshToken) {
+    const client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
+    client.setCredentials({ refresh_token: refreshToken });
+    return google.drive({
+      version: GOOGLE_DRIVE_API_VERSION,
+      auth: client,
+    });
+  }
 
-	createFolder = folderName => {
-		return this.driveClient.files.create({
-			resource: {
-				name: folderName,
-				mimeType: 'application/vnd.google-apps.folder',
-			},
-			fields: 'id, name'
-		});
-	}
+  createFolder(folderName) {
+    return this.driveClient.files.create({
+      resource: {
+        name: folderName,
+        mimeType: 'application/vnd.google-apps.folder',
+      },
+      fields: 'id, name',
+    });
+  }
 
-	searchFolder = folderName => {
-		return new Promise((resolve, reject) => {
-			this.driveClient.files.list(
-				{
-					q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
-					fields: 'files(id, name)',
-				},
-				(err, res) => {
-					if (err) {
-					  return reject(err);
-					}
-		  
-					return resolve(res.data.files ? res.data.files[0] : null);
-				},
-			);
-		});
-	}
+  searchFolder(folderName) {
+    return new Promise((resolve, reject) => {
+      this.driveClient.files.list(
+        {
+          q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}'`,
+          fields: 'files(id, name)',
+        },
+        (err, res) => {
+          if (err) {
+            return reject(err);
+          }
 
-	saveFile = (fileName, filePath, fileMimeType, folderId) => {
-		
-		return this.driveClient.files.create({
-		  requestBody: {
-			name: fileName,
-			mimeType: fileMimeType,
-			parents: folderId ? [folderId] : [],
-		  },
-		  media: {
-			mimeType: fileMimeType,
-			body: fs.createReadStream(filePath),
-		  },
-		});
-	}
-}
+          return resolve(res.data.files ? res.data.files[0] : null);
+        },
+      );
+    });
+  }
+
+  saveFile(fileName, filePath, fileMimeType, folderId) {
+    return this.driveClient.files.create({
+      requestBody: {
+        name: fileName,
+        mimeType: fileMimeType,
+        parents: folderId ? [folderId] : [],
+      },
+      media: {
+        mimeType: fileMimeType,
+        body: fs.createReadStream(filePath),
+      },
+    });
+  }
+};

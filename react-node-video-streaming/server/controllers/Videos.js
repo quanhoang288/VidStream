8a6a0f7a6548f51666b88ca1727a8e730827a45f
quanhoang288 <quanpro288@gmail.com>
2021-12-01 02:18:08 +0700

    update video controller and asset for storing video info to db

--- react-node-video-streaming/server/controllers/Videos.js
+++ react-node-video-streaming/server/controllers/Videos.js
@@ -1,31 +1,79 @@
-const VideoModel = require('../models/Videos');
-const httpStatus = require('../utils/httpStatus');
 const fs = require('fs');
 const path = require('path');
+const VideoModel = require('../models/Videos');
+const AssetModel = require('../models/Assets');
+const httpStatus = require('../utils/httpStatus');
+const {
+  ASSET_TYPE_CHUNK,
+  ASSET_TYPE_MANIFEST,
+  ASSET_TYPE_THUMBNAIL,
+  VIDEO_STATUS_DRAFT,
+} = require('../constants/constants');
 const { encode, generateThumbnail } = require('../services/ffmpeg');
 const { uploadFolderToDrive } = require('../utils/cloudUpload');
+const { ASSET_DIR } = require('../configs');
 
 const videoController = {};
 
 videoController.upload = async (req, res) => {
-    try {
-        const video = req.file;
-        
-        //generate thumbnail and encode video in DASH format
-        const thumbnail = await generateThumbnail(video);
-        const storageDir = await encode(video);
-        
-        // upload generated files to google drive
-        const uploadedFiles = await uploadFolderToDrive(storageDir, storageDir.split('/')[1]);
-        
-        //TODO: save in DB after succesful upload
-        res.status(httpStatus.OK)
-            .json({ message: 'Upload successfully!' });
-    } catch (err) {
-        console.log(err);
-        res.status(httpStatus.INTERNAL_SERVER_ERROR)
-            .json({ error: err.message });
-    }
-}
+  try {
+    const videoFile = req.file;
+
+    // generate thumbnail and encode video in DASH format
+    const thumbnail = await generateThumbnail(videoFile);
+    const { targetDir, targetFn } = await encode(videoFile);
+
+    // move manifest file to asset folder
+    const manifestFileName = path.basename(targetFn);
+    fs.renameSync(
+      path.resolve(targetFn),
+      path.resolve(path.join(ASSET_DIR, manifestFileName)),
+    );
+
+    // upload generated chunks to google drive
+    const uploadedFiles = await uploadFolderToDrive(
+      targetDir,
+      targetDir.split('/')[1],
+    );
+
+    // store in database
+    const manifestAsset = new AssetModel({
+      type: ASSET_TYPE_MANIFEST,
+      fileName: manifestFileName,
+    });
+    const thumbnailAsset = new AssetModel({
+      type: ASSET_TYPE_THUMBNAIL,
+      fileName: thumbnail.filename,
+    });
+    const savedManifest = await manifestAsset.save();
+    const savedThumbnail = await thumbnailAsset.save();
+
+    const video = new VideoModel({
+      manifestFile: savedManifest._id,
+      thumbnail: savedThumbnail._id,
+      status: VIDEO_STATUS_DRAFT,
+    });
+    const savedVideo = await video.save();
+
+    const chunkAssets = uploadedFiles.map(
+      (file) =>
+        new AssetModel({
+          type: ASSET_TYPE_CHUNK,
+          fileName: file.name,
+          remoteId: file.id,
+          video: savedVideo._id,
+        }),
+    );
+    const savePromises = chunkAssets.map((asset) => asset.save());
+    await Promise.all(savePromises);
+
+    res.status(httpStatus.CREATED).json({ data: savedVideo });
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Error uploading video!' });
+  }
+};
 
 module.exports = videoController;
